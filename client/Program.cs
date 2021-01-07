eb4da0158659eeb98aee28fa174d071946500dcf
darioberg <62504970+darioberg@users.noreply.github.com>
2021-01-07 14:35:11 +0100

    unfinished MQTT

--- client/Program.cs
+++ client/Program.cs
@@ -4,7 +4,12 @@ using Client.Sensors;
 using System.Net;
 using System.IO;
 using System.Collections;
-
+using System.Threading.Tasks;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet;
+using CsQuery.Engine.PseudoClassSelectors;
+using MQTTnet.Client;
 
 namespace Client
 {
@@ -23,9 +28,9 @@ namespace Client
             {
                 foreach (SensorInterface sensor in sensors)
                 {
-                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://b1df610c81ab.ngrok.io/scooters/1");
-                    httpWebRequest.ContentType = "text/json";
-                    httpWebRequest.Method = "POST";
+                    //HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create("http://b1df610c81ab.ngrok.io/scooters/1");
+                    //httpWebRequest.ContentType = "text/json";
+                    //httpWebRequest.Method = "POST";
 
                     using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
                     {
@@ -45,6 +50,43 @@ namespace Client
             }
 
         }
+
+        public static async Task ConnectAsync()
+        {
+            string clientId = Guid.NewGuid().ToString();
+            string mqqtURI = "test.mosquitto.org";
+            int mqttPort = 1883;
+            bool mqttSecure = false;
+
+            var messageBuilder = new MqttClientOptionsBuilder()
+                .WithClientId(clientId)
+                .WithTcpServer(mqqtURI, mqttPort)
+                .WithCleanSession();
+
+            var options = mqttSecure
+                ? messageBuilder
+                .WithTls()
+                .Build()
+                : messageBuilder
+                .Build();
+
+            var managedOptions = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(options)
+                .Build();
+
+            var client = new MqttFactory().CreateManagedMqttClient();
+
+            await client.StartAsync(managedOptions);
+        }
+
+        public static async void PublishAsync(IMqttClient mqttClient)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic("iot2021/es2/dariobergamasco")
+                .WithPayload();
+        }
+            
 
     }
 
